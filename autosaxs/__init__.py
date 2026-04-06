6b2cf245a3dce879db792e1d05e5d385fc3a0447
mikhail <mikhailkulkov11@gmail.com>
2026-04-06 12:44:43 +0300

    fix commit script

--- autosaxs/__init__.py
+++ autosaxs/__init__.py
@@ -1,5 +1,5 @@
 # autosaxs: interactive SAXS pipeline core (controller, processor, skill, utils, guinier, event_bus, cli, viewer, context, gui, gui_interface, api, polydispfit, report)
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 import re
 import subprocess
 
