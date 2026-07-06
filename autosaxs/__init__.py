fbe12f67dbb03e318a990251576b3d8825647540
mikhail <mikhailkulkov11@gmail.com>
2026-07-06 12:09:25 +0300

    simplify guisaxs update

--- autosaxs/__init__.py
+++ autosaxs/__init__.py
@@ -1,5 +1,5 @@
 # autosaxs: interactive SAXS pipeline core (controller, processor, skill, utils, guinier, event_bus, cli, viewer, context, gui, gui_interface, api, polydispfit, report)
-__version__ = "2.7.4"
+__version__ = "2.7.5"
 import re
 import subprocess
 import warnings
